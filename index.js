c4bb08b6006febf5b83ecb25f289192ef7f40a65
Jade <fungj@reed.edu>
2019-06-11 16:32:26 -0700

    added ability to add children to other nodes
    eg adding a character to a set like [a]

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const rs = require('randomstring');
 
+const unrecognizedTypeError = new Error('unrecognized type');
+const canAcceptChild = ({ parent, type }) => {
+  switch (parent.type) {
+    case ('char'): {
+      return false;
+    }
+    case ('set'): {
+      switch (type) {
+        case ('char'): {
+          return true;
+        }
+        default: {
+          throw unrecognizedTypeError;
+        }
+      }
+    }
+    default: {
+      throw unrecognizedTypeError;
+    }
+  }
+};
+
 class RegexObj {
   constructor() {
     this.nodes = {};
@@ -7,23 +29,42 @@ class RegexObj {
     this.nodeCount = 0;
   }
 
-  addNode({ type, contents }) {
+  // addNode will insert a new regex part into the class' data
+  // structures
+  addNode({ type, contents, parent }) {
     // init node object
     const nodeName = `${type}_${rs.generate({ length: 8, capitalization: 'lowercase' })}`;
     const node = {
       type,
+      name: nodeName,
       contents,
       position: this.nodeCount,
     };
+    if (parent) {
+      const parentNode = this.nodes[parent];
+      // if parent arg specified, check to see if parent node
+      // can accept children
+      const validChild = canAcceptChild({
+        parent: parentNode,
+        type,
+      });
+      if (validChild === false) throw new Error(`cannot set ${type} as child of ${parentNode.type}`);
+      // if parent can accept node as child, modify both
+      node.parent = parent;
+      if (!parentNode.children) parentNode.children = [];
+      parentNode.children.push(nodeName);
+    }
     // add node to regex object
     this.nodeList.push(nodeName);
     this.nodes[nodeName] = node;
     this.nodeCount += 1;
     // return node info
     const res = {
+      name: nodeName,
       position: this.nodeList.indexOf(nodeName),
       type: this.nodes[nodeName].type,
       contents: this.nodes[nodeName].contents,
+      parent: this.nodes[nodeName].parent,
     };
     return res;
   }
@@ -31,8 +72,6 @@ class RegexObj {
 
 const regexObj = {
   RegexObj,
-  patternRoot: 'this is the beginning of the pattern',
-  patternEnd: 'this is the end of the pattern',
 };
 
 module.exports = regexObj;
--- index.test.js
+++ index.test.js
@@ -7,7 +7,7 @@ test('new RegexObj() creates RegexObj instance', () => {
   expect(regex.nodeCount).toEqual(0);
 });
 
-test('addNode() should add a node', () => {
+test('addNode() should add a character node', () => {
   const regex = new regexObj.RegexObj();
   const node = regex.addNode({
     type: 'char',
@@ -24,6 +24,34 @@ test('addNode() should add a node', () => {
   expect(node.position).toEqual(0);
   expect(node.type).toEqual('char');
   expect(node.contents).toEqual('a');
-  expect(node.precededBy).toEqual(regexObj.PatternRoot);
-  expect(node.followedBy).toEqual(regexObj.PatternEnd);
+  expect(node.name).toEqual(regex.nodeList[0]);
+});
+
+// child validity testing, tests canAcceptChild function
+test('addNode() should not allow adding char node to char nodes children', () => {
+  const regex = new regexObj.RegexObj();
+  const charNode1 = regex.addNode({
+    type: 'char',
+    contents: 'c',
+  });
+  expect(() => regex.addNode({
+    type: 'char',
+    contents: 'd',
+    parent: charNode1.name,
+  })).toThrow('cannot set char as child of char');
+});
+
+// adding a character as a child to a set
+test('addNode() should add a set with a character in it', () => {
+  const regex = new regexObj.RegexObj();
+  const setNode = regex.addNode({
+    type: 'set',
+  });
+  const charNode = regex.addNode({
+    type: 'char',
+    contents: 'b',
+    parent: setNode.name,
+  });
+  expect(charNode.parent).toEqual(setNode.name);
+  expect(regex.nodes[setNode.name].children).toEqual([charNode.name]);
 });
